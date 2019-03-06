0ce28a2468c129638eb26d1f9542db7260649f8c
David Mason <davidm@tcdev>
2019-03-06 20:24:23 +0000

    wip (500 error)

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Transitional repo for TalentCloud docker integration.
+
+Rebuild from scratch: in this directory, with git and docker-compose installed locally execute the following (from a bash-type shell):
+
+`rm -rf TalentCloud && git clone --single-branch -b dev https://github.com/GCTC-NTGC/TalentCloud.git; docker-compose build && docker-compose run`
+
+After making a hosts entry for talent.local.ca, you should be able to access the application at https://talent.local.ca/
+
+
---
+++ op/op-assert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+BASE=/opt/TalentCloud/var/www
+
+if [ -f $BASE/.setup ]; then
+  echo -n talentcloud has a setup file from `cat $BASE/.setup`
+else 
+  echo "running talentcloud-op setup via op-assert.sh"
+  mkdir /var/log/nginx/
+  make gen-certs env composer-install laravel-init fresh-db fake-data npm-install npm-dev
+  # FIXME this should use nginx UUID
+  chmod -R 777 $BASE/bootstrap/cache
+  chmod -R 777 $BASE/storage
+  date > $BASE/.setup
+fi
+
+# wait around for op commands
+sleep 365d
