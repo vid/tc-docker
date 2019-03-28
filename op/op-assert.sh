50049717b76838d561fd325b1a619be2d72b279c
Yonathan <yonidoe15@gmail.com>
2019-03-28 14:18:31 -0400

    added comment

--- op/op-assert.sh
+++ op/op-assert.sh
@@ -1,17 +1,18 @@
-#!/bin/sh 
+#!/bin/sh
 BASE=/opt/TalentCloud/var/www
 
 if [ -f $BASE/.setup ]; then
   echo -n talentcloud has a setup file from `cat $BASE/.setup`
-else 
+else
   echo "running talentcloud-op setup via op-assert.sh"
   if [ ! -d /var/log/nginx ]; then
     mkdir /var/log/nginx/
   fi && \
   # FIXME commands other than gen-certs should be run as non-root user
   if [ "${DOCKER_TOOLBOX_INSTALL_PATH}" ]; then
-    make gen-certs env composer-install laravel-init fresh-db fake-data npm-cross-env npm-install npm-dev 
-  else  
+    # For Docker Toolbox
+    make gen-certs env composer-install laravel-init fresh-db fake-data npm-cross-env npm-install npm-dev
+  else
     make gen-certs env composer-install laravel-init fresh-db fake-data npm-install-cross-env-global npm-install-no-bin-links npm-dev
   fi && \
   for i in storage bootstrap/cache; do
